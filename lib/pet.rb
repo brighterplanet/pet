bd59f301f1a7e373a37d071841e69b9778ef3cec
Derek Kastner <dkastner@gmail.com>
2010-08-12 15:12:37 -0400

    Using emitter gem

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,23 +1,10 @@
+require 'emitter'
+
 module BrighterPlanet
   module Pet
-    extend self
+    extend BrighterPlanet::Emitter
 
-    def included(base)
-      require 'cohort_scope'
-      require 'falls_back_on'
-      require 'falls_back_on/active_record_ext'
-
-      require 'pet/carbon_model'
-      require 'pet/characterization'
-      require 'pet/data'
-      require 'pet/summarization'
-
-      base.send :include, BrighterPlanet::Pet::CarbonModel
-      base.send :include, BrighterPlanet::Pet::Characterization
-      base.send :include, BrighterPlanet::Pet::Data
-      base.send :include, BrighterPlanet::Pet::Summarization
-    end
-    def pet_model
+    def self.pet_model
       if Object.const_defined? 'Pet'
         ::Pet
       elsif Object.const_defined? 'PetRecord'
